e2b71d4fd98f355182b4a78f209c8d1d884be5a4
akumo <akumo@yahoo.com>
2016-03-12 14:41:26 +0700

    Get event onClick finish

--- app/src/main/java/com/akumoshop/akumo/mytraffic/MainActivity.java
+++ app/src/main/java/com/akumoshop/akumo/mytraffic/MainActivity.java
@@ -1,13 +1,58 @@
 package com.akumoshop.akumo.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit -> declare variable
+    private ListView trafficListView;
+    private Button aboutmeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind widget
+        bindWidget();
+
+        //Button Controller
+        //alt+enter to create method
+        buttonController();
+
+
+    } // Main Method
+
+    private void buttonController() {
+        //ctrl+space to show collection of new method (new View.OnClickListener)
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                //ctrl+space to automatic create variable
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.lamp);
+                mediaPlayer.start();
+
+            } //onClick
+        });
+
+    } //buttonController
+
+    private void bindWidget() {
+        //alt+enter to show "cast to"
+        //ctrl+space to suggest word
+        //ctrl+shift+enter to completement statement
+        //ctrl+alt+L to auto format
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutmeButton = (Button) findViewById(R.id.button);
+
+    } //bindWidget
+
+} // Main Class
